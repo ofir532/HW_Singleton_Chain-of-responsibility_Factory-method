c4a6749bfa95d43f06c5dfcd7c6f13b48e38fee2
Ofir Dahan <ofirxd532@gmail.com>
2022-05-17 22:48:49 +0300

    Update

---
+++ HW_Singleton_Chain of responsibility_Factory method/Factory/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Singleton_Chain_of_responsibility_Factory_method.Factory
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+    }
+}
---
+++ HW_Singleton_Chain of responsibility_Factory method/Factory/VegetarianDiet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Singleton_Chain_of_responsibility_Factory_method.Factory
+{
+    internal class VegetarianDiet
+    {
+    }
+}
